342391d1d8a73bc9f04b2c0ab3048bf9e6de38fb
450 G1 <ibraheemelfakharany@gmail.com>
2018-09-19 22:59:09 +0200

    add button in MainActivity

--- app/src/main/java/app/fakharany/com/gittree/MainActivity.kt
+++ app/src/main/java/app/fakharany/com/gittree/MainActivity.kt
@@ -1,12 +1,17 @@
 package app.fakharany.com.gittree
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Toast.makeText(this, "message", Toast.LENGTH_SHORT).show()
+        }
     }
 }
